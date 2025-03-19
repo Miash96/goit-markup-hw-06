da6d9b72b914c85a93e1440860b59db00dd21353
Mi_Keks <mihakekuh@gmail.com>
2025-03-19 10:43:51 +0200

    Homework 6

--- js/script.js
+++ js/script.js
@@ -8,3 +8,27 @@ function documentClick(e) {
 		document.documentElement.classList.toggle('menu-open');
 	}
 }
+
+// const openModal = document.getElementById('openModal');
+// const closeModal = document.getElementById('closeModal');
+// const modal = document.getElementById('modal');
+// const overlay = document.getElementById('modalOverlay');
+// const body = document.body;
+
+
+// openModal.addEventListener('click', function (event) {
+// 	event.preventDefault();
+// 	modal.style.display = 'block';
+// 	overlay.style.display = 'block';
+// 	body.classList.add('modal-open');
+// });
+
+
+// function close() {
+// 	modal.style.display = 'none';
+// 	overlay.style.display = 'none';
+// 	body.classList.remove('modal-open');
+// }
+
+// closeModal.addEventListener('click', close);
+// overlay.addEventListener('click', close);
